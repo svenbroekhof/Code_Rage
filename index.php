f27b740253200eb1e2ebece821b4a0a25f8e4aba
IJuanTM <iwan2510@gmail.com>
2019-09-09 13:09:46 +0200

    Index en andere start dingen

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Include the bootstrap css and meta links. -->
+    <?php
+    include("./index/meta.php");
+    include("./index/css.php");
+    ?>
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <h1>Hello, world!</h1>
+
+    <!-- Include the needed scripts. -->
+    <?php include("./index/js.php"); ?>
+</body>
+
+</html>
---
+++ index/js.php
@@ -0,0 +1,7 @@
+<!-- Java script files, JQuery, Popper, Bootstrap. -->
+<script src="./js/jquery.min.js"></script>
+<script src="./js/popper.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+
+<!-- Custom Java script files -->
+<script src="./js/script.js"></script>
