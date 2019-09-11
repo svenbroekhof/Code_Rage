ea3ad833535b4f2dea38af3f978474f8fad4b894
IJuanTM <iwan2510@gmail.com>
2019-09-11 12:13:27 +0200

    sidebar navbar enzo

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ $("#menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+
+$(function () {
+    $("sidebar-collapse").click(function () {
+        var $spin = $("#menu-toggle");
+        $spin.removeClass("spinEffect");
+        setTimeout(function () {
+            $spin.addClass("spinEffect")
+        }, 0);
+    });
+});
--- layout/navbar.php
+++ layout/navbar.php
@@ -1,29 +1,26 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-    <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+    <a class="sidebar-collapse btn" id="menu-toggle"><i class="fas fa-chevron-left fa-2x"></i></a>
+
+    <div class="navbar-heading d-md-flex d-block flex-row mx-md-auto mx-0">
+        <a href="#"><img class="nav-logo" src="./img/logo.png"></a>
+    </div>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
         <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
+
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#"><i class="fas fa-sign-in-alt"></i> Login</a>
             </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
+
+            <li class="nav-item active">
+                <a class="btn btn-primary   nav-link" href="#"><i class="fas fa-address-card"></i> Register</a>
             </li>
+
         </ul>
     </div>
 </nav>
