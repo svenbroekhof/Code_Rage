853734a0ec85fca42f6c84fdebb51957e3617080
Sven Broekhof <322632@student.mboutrecht.nl>
2019-10-14 13:28:52 +0200

    Project aangemaakt

--- index/js.php
+++
@@ -1,8 +0,0 @@
-<!-- Java script files, JQuery, Popper, Bootstrap. -->
-<script src="./js/jquery-3.3.1.min.js"></script>
-<script src="./js/popper.min.js"></script>
-<script src="./js/bootstrap.min.js"></script>
-
-<!-- Custom Java script files -->
-<script src="./js/script.js"></script>
-<script src="./js/icons.js"></script>
--- js/script.js
+++
@@ -1,8 +0,0 @@
-$("#menu-toggle").click(function (e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
-
-$(".rotate").click(function () {
-    $(this).toggleClass("down");
-});
