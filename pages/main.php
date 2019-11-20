17ee96f4ebb2ccc5c6601f4395eca126eb262f5b
svenbroekhof <svenbroekhof@gmail.com>
2019-11-20 01:36:53 +0100

    Banner added

--- pages/main.php
+++ pages/main.php
@@ -1,4 +1,13 @@
-<img src="./img/moon.png" class="img-fluid" id="bannerimage" alt="Responsive image" style="height: 200px; width: 100%;">
+<div class="jumbotron jumbotron-fluid" id="bannerimage">
+            <div class="container"id="HalloJumbo">
+              <h3>Code rage</h3>
+              <input type="text" class="form-control" placeholder="Zoek" aria-label="text" aria-describedby="basic-addon1"> <div class="modal-footer">
+                  <button type="button" class="btn btn-outline-primary">Zoek</button>
+              </div>
+            </div>
+        </div>
+
+<!-- <img src="./img/moon.png" class="img-fluid" id="bannerimage" alt="Responsive image" style="height: 200px; width: 100%;"> -->
 <!-- <div class="row">
     <div class="col-sm-12" id="main_tag">
         <div class="row">
