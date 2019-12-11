a747dc128417842ff53a0ea5b531adc1f1f7661d
Noah Nap <321833@student.mboutrecht.nl>
2019-12-11 01:30:01 +0100

    changes to the register file

--- back_end/connect_db.php
+++ back_end/connect_db.php
@@ -5,7 +5,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'knowledgebank ');
+define('DB_NAME', 'knowledgebank');
 
 /* Attempt to connect to MySQL database */
 try{
--- back_end/register-script.php
+++ back_end/register-script.php
@@ -70,79 +70,82 @@ require_once "connect_db.php";
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
+$email =
 
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Validate username
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT id FROM users WHERE username = :username";
+// Validate username
+if (empty(trim($_POST["username"]))) {
+    $username_err = "Please enter a username.";
+} else {
+    // Prepare a select statement
+    $sql = "SELECT user_id FROM login WHERE username = :username";
 
-        if($stmt = $pdo->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+    if ($stmt = $pdo->prepare($sql)) {
+        // Bind variables to the prepared statement as parameters
+        $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
 
-            // Set parameters
-            $param_username = trim($_POST["username"]);
+        // Set parameters
+        $param_username = trim($_POST["username"]);
 
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                if($stmt->rowCount() == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
+        // Attempt to execute the prepared statement
+        if ($stmt->execute()) {
+            if ($stmt->rowCount() == 1) {
+                $username_err = "This username is already taken.";
+            } else {
+                $username = trim($_POST["username"]);
             }
+        } else {
+            echo "Oops! Something went wrong. Please try again later.";
         }
-
-        // Close statement
-        unset($stmt);
     }
 
+    // Close statement
+    unset($stmt);
+}
+
     // Validate password
-    if(empty(trim($_POST["password"]))){
+    if (empty(trim($_POST["password"]))) {
         $password_err = "Please enter a password.";
-    } elseif(strlen(trim($_POST["password"])) < 6){
+    } elseif (strlen(trim($_POST["password"])) < 6) {
         $password_err = "Password must have atleast 6 characters.";
-    } else{
+    } else {
         $password = trim($_POST["password"]);
     }
 
     // Validate confirm password
-    if(empty(trim($_POST["validate_password"]))){
+    if (empty(trim($_POST["validate_password"]))) {
         $confirm_password_err = "Please confirm password.";
-    } else{
+    } else {
         $confirm_password = trim($_POST["validate_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
+        if (empty($password_err) && ($password != $confirm_password)) {
             $confirm_password_err = "Password did not match.";
         }
     }
 
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
 
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+        $sql = "INSERT INTO users (email,password, username, activation_code, role) VALUES (:email, :password, :username, :activation_code, :role)";
 
-        if($stmt = $pdo->prepare($sql)){
+        if ($stmt = $pdo->prepare($sql)) {
             // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
             $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
 
             // Set parameters
+            $param_email = $email;
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
 
             // Attempt to execute the prepared statement
-            if($stmt->execute()){
+            if ($stmt->execute()) {
                 // Redirect to login page
                 header("location: login.php");
-            } else{
+            } else {
                 echo "Something went wrong. Please try again later.";
             }
         }
