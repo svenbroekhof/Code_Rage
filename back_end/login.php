8f9849dfdc9afe6a5fc7fa9d07bed35faab50f93
Sven Broekhof <322632@student.mboutrecht.nl>
2020-01-24 12:16:24 +0100

    changes to login.php

--- back_end/login.php
+++ back_end/login.php
@@ -5,14 +5,10 @@ if(isset($_POST['username'], $_POST['password'])){
     $pass = hash('sha256', $_POST['password']);
     $user = $_POST['username'];
 
-    $query = dbConnect()->prepare("SELECT username, password, user_id FROM login WHERE username=:username AND password=:password");
+    $query = dbConnect()->prepare("SELECT username, password, user_id, role FROM login WHERE username=:username AND password=:password");
     $query->bindParam(':username', $user);
     $query->bindParam(':password', $pass);
     $query->execute();
-
-    $roles = dbConnect()->prepare("SELECT role FROM login WHERE role=:role");
-    $roles->bindParam(':role', $role);
-    $roles->execute();
 
     var_dump($query);
     var_dump($roles);
@@ -24,10 +20,9 @@ if(isset($_POST['username'], $_POST['password'])){
         $_SESSION['role'] = 1;
         session_start();
         $_SESSION['username'] = $_POST['username'];
-        $_SESSION['role'] = $_POST['role'];
-
-
+        $_SESSION['role'] = $row['role'];
         $_SESSION['user_id'] = $row['user_id'];
+
        header("Refresh: 1; URL=../index.php?content=dashboard_student");
 
     }
